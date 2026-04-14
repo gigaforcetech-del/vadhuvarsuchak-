69adfd843ad0048565cc796ad68b13e8284ed421
gigaforcetech-dele <gigaforcetech@gmail.com>
2026-04-14 15:37:33 +0530

    login page

---
+++ credentials.js
@@ -0,0 +1,9 @@
+// Static Login Credentials
+// Add or modify users here
+
+const USERS = [
+  { username: "admin", password: "junnar@123" },
+  { username: "rahul", password: "rahul2026" },
+  { username: "snehal", password: "snehal2026" },
+  { username: "user1", password: "pass1234" }
+];
